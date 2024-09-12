3e21e2ef232aecc90c60864c63005b44130bb9de
Esteban Madrid <misaa023@gmail.com>
2024-09-12 13:19:42 -0300

    finalizaión del capitulo 3

--- 03-Strings/js/06-app.js
+++ 03-Strings/js/06-app.js
@@ -2,9 +2,14 @@
 
 const producto = ' Monitor 20 Pulgadas ';
 
+const bebidas = 'Coca Cola, Agua, Té';
+
 console.log(producto.repeat(3));
 
 // TODO: split -> convierte la cadena en un array
 // split recibe un parametro que es la cadena por la que se va a dividir
 
 console.log(producto.split(' '));
+
+// separar por coma
+console.log(bebidas.split(','));
---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,14 @@
+// TODO: CONVERTIR EN MAYUSCULAS Y MINUSCULAS
+
+const producto = 'Monitor 20 Pulgadas';
+
+// toUpperCase -> convierte la cadena de texto a mayusculas
+// toLowerCase -> convierte la cadena de texto a minusculas
+
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+// convertir un numero a string
+const precio = 300;
+
+console.log(precio.toString());
